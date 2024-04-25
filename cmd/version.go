e0405360c91fee2f78bdbeb007e47a5c18d5f0ce
nomysz <nomysz@yahoo.com>
2024-04-25 19:53:24 +0200

    Add version 0.1.0 to changelog

--- cmd/version.go
+++ cmd/version.go
@@ -6,11 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const Version = "0.1.0"
+
 var VersionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Send remidners via configured handlers",
-	Long:  "Send remidners via configured handlers",
+	Short: "Show app version",
+	Long:  "Show app version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("celebrations v1.0.0")
+		fmt.Println(fmt.Sprintf("celebrations v%s", Version))
 	},
 }
